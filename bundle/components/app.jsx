96a3d3d8097d22f132ab2c2ca75ef6d9b0d2085e
Tyler Torola <tyler@tjt.codes>
2016-09-12 19:00:23 -0700

    builds basic app state

--- bundle/components/app.jsx
+++ bundle/components/app.jsx
@@ -5,11 +5,42 @@ import Info          from './info';
 import Search        from './search';
 import SearchResults from './search_results';
 
-export default () => (
-	<div className="App">
-		<Map />
-		<Info />
-		<Search />
-		<SearchResults />
-	</div>
-);
+export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			locations: [],
+			markers: [],
+			selected: {},
+			query: "",
+			results: []
+		}
+	}
+
+	setQuery() {
+
+	}
+
+	selectMovie() {
+
+	}
+
+	render() {
+		let { locations, markers, selected, results } = this.state;
+
+		return (
+			<div className="App">
+				<Map
+					markers={ markers } 
+					selectMovie={ this.selectMovie } />
+				<Info
+					selected={ selected } />
+				<Search
+					setQuery={ this.setQuery } />
+				<SearchResults
+					results={ results } />
+			</div>
+		)
+	}
+}
