4d85a73311388b4a781771c867e223361202bf4b
Tyler Torola <tyler@tjt.codes>
2016-09-12 18:52:41 -0700

    implaments basic google map

--- bundle/components/map.jsx
+++ bundle/components/map.jsx
@@ -1,6 +1,24 @@
-import React         from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-export default () => (
-	<section className="Map">
-	</section>
-)
+class Map extends React.Component {
+	componentDidMount() {
+		let map = ReactDOM.findDOMNode(this.refs.map);
+		let options = {
+			center: { lat: 37.699, lng: -122.313 },
+			zoom: 11
+		};
+
+		this.map = new google.maps.Map(map, options);
+	}
+
+	render() {
+		return (
+			<div className="Map">
+				<div className="MapView" ref="map"/>
+			</div>
+		);
+	}
+}
+
+export default Map;
